ddf08d308898df88e6f364e722e2e6c64c22c5b1
samahghanem255-art <samahghanem255@gmail.com>
2026-05-02 20:04:11 +0100

    Add basic CSS styles for body and navigation

---
+++ style.cs
@@ -0,0 +1,16 @@
+body {
+  font-family: Arial;
+  text-align: center;
+  margin-top: 50px;
+}
+
+nav {
+  margin-top: 20px;
+}
+
+nav a {
+  margin: 10px;
+  text-decoration: none;
+  color: blue;
+  font-size: 18px;
+}
